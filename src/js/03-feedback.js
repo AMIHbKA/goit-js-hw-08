69dac47770935cfe2b845ea33a0d580aa45f7f83
Олександр Шутяк <bishop0013forum@gmail.com>
2023-02-23 17:00:29 +0200

    tsk#3 begin

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import Throttle from 'lodash.throttle';
 
+const LOCALSTORAGEKEY = 'feedback-form-state';
+const 
+
+
+
+const refs = {
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+};
+
+// const storageChange = window.addEventListener('storage');
+
+const onLocalStorageChange = e => {
+    console.log(e.currentTarget.value);
+    
+};
+
+refs.email.addEventListener('input', onLocalStorageChange);
